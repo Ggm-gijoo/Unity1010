e401eeaa38fddc97789c67551b5c57da4dc9ed89
Ggm-gijoo <gijoo1012@gmail.com>
2022-06-20 20:19:26 +0900

    2022/06/20
    일시정지 구현

---
+++ Assets/01_Scripts/PauseManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject imageBackGroundOverlay;
+    [SerializeField]
+    private Animator panelAnim;
+
+    public void OnAppear()
+    {
+        imageBackGroundOverlay.SetActive(true);
+        gameObject.SetActive(true);
+        panelAnim.Play("OnPanel");
+        gameObject.GetComponent<CanvasGroup>().alpha = 1;
+    }
+
+    public void OnDisappear()
+    {
+        panelAnim.Play("OffPanel");
+    }
+
+    public void EndOfDisappear()
+    {
+        imageBackGroundOverlay.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
